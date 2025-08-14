241b44e390fcb824f5d5c20da951c190aad83321
Vladimir Bajenov <vladimir@bajenov.info>
2025-08-15 00:21:33 +0400

    Config example

--- config/livewire-section-builder.php
+++ config/livewire-section-builder.php
@@ -1,6 +1,25 @@
 <?php
 
-// config for MountainClans/LivewireSectionBuilder
 return [
-
+    'sections_sets' => [
+//        'main_page' => [
+//            [
+//                'key' => 'top_banner',
+//                'title' => 'Top banner',
+//                'model' => EloquentSectionModelClass::class,
+//                'editor' => LivewireEditorComponent::class,
+//                'view' => LivewireViewComponent::class,
+//            ],
+//            [
+//                'key' => 'advantages',
+//                'title' => 'Advantages',
+//                'model' => EloquentSectionModelClass::class,
+//                'editor' => LivewireEditorComponent::class,
+//                'view' => LivewireViewComponent::class,
+//            ]
+//        ],
+//        'site_rules' => [
+//            //
+//        ],
+    ],
 ];
