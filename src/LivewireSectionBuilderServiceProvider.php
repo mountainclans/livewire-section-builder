77ae9e11cb418b5aeec66baaed19a7eab253699f
Vladimir Bajenov <vladimir@bajenov.info>
2025-08-15 00:21:49 +0400

    ServiceProvider config

--- src/LivewireSectionBuilderServiceProvider.php
+++ src/LivewireSectionBuilderServiceProvider.php
@@ -4,22 +4,15 @@ namespace MountainClans\LivewireSectionBuilder;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use MountainClans\LivewireSectionBuilder\Commands\LivewireSectionBuilderCommand;
 
 class LivewireSectionBuilderServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('livewire-section-builder')
             ->hasConfigFile()
             ->hasViews()
-            ->hasMigration('create_livewire_section_builder_table')
-            ->hasCommand(LivewireSectionBuilderCommand::class);
+            ->hasMigration('create_livewire_section_builder_table');
     }
 }
