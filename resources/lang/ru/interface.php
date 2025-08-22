88b61773c7b81bf66cdbf45e3c95098a18615004
Vladimir Bajenov <vladimir@bajenov.info>
2025-08-23 00:54:38 +0400

    Translations

---
+++ resources/lang/ru/interface.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'sections' => 'Секции',
+    'add_section' => 'Добавить секцию',
+    'add' => 'Добавить',
+    'type_of_section_to_add' => 'Тип секции для добавления',
+    'select_type_of_section_to_add' => 'Выберите тип секции для добавления',
+    'sections_list_is_empty_yet' => 'Секции ещё не добавлены',
+];
