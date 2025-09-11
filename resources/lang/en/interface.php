0f3bf629168943617bc22e81b65860dfe1ef6f1f
Vladimir Bajenov <vladimir@bajenov.info>
2025-09-11 17:39:33 +0400

    Translations

---
+++ resources/lang/en/interface.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'sections' => 'Sections',
+    'add_section' => 'Add section',
+    'add' => 'Add',
+    'type_of_section_to_add' => 'Type of section to add',
+    'select_type_of_section_to_add' => 'Select the type of section to add',
+    'sections_list_is_empty_yet' => 'No sections have been added yet',
+    'sure_delete_this_section' => 'Are you sure you want to delete this section?',
+    'no_section_configured' => 'Sections for a page with this template do not exist in the configuration.',
+];
---
+++ resources/lang/es/interface.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'sections' => 'Secciones',
+    'add_section' => 'Agregar sección',
+    'add' => 'Agregar',
+    'type_of_section_to_add' => 'Tipo de sección para agregar',
+    'select_type_of_section_to_add' => 'Seleccione el tipo de sección para agregar',
+    'sections_list_is_empty_yet' => 'Aún no se han agregado secciones',
+    'sure_delete_this_section' => '¿Está seguro de que desea eliminar esta sección?',
+    'no_section_configured' => 'No existen secciones en la configuración para una página con esta plantilla.',
+];
